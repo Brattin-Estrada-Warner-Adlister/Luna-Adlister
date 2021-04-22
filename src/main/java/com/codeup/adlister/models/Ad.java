7db12c1708a7dc42316d6d5febd96aa3832b2382
audiesha brattin <a.brattin@gmail.com>
2021-04-22 13:43:07 -0500

    adding

--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -1,53 +1,75 @@
 package com.codeup.adlister.models;
 
+import java.util.List;
+
 public class Ad {
-    private long id;
-    private long userId;
-    private String title;
-    private String description;
+  private long id;
+  private long userId;
+  private String title;
+  private String description;
+  private List<String> categories;
 
-    public Ad(long id, long userId, String title, String description) {
-        this.id = id;
-        this.userId = userId;
-        this.title = title;
-        this.description = description;
-    }
+  public Ad(long id, long userId, String title, String description) {
+    this.id = id;
+    this.userId = userId;
+    this.title = title;
+    this.description = description;
+  }
 
-    public Ad(long userId, String title, String description) {
-        this.userId = userId;
-        this.title = title;
-        this.description = description;
-    }
+  public Ad(long userId, String title, String description, List<String> categories) {
+    this.userId = userId;
+    this.title = title;
+    this.description = description;
+    this.categories = categories;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public Ad(long id, long userId, String title, String description, List<String> categories) {
+    this.id = id;
+    this.userId = userId;
+    this.title = title;
+    this.description = description;
+    this.categories = categories;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public Ad(long userId, String title, String description) {
+    this.userId = userId;
+    this.title = title;
+    this.description = description;
+  }
 
-    public long getUserId() {
-        return userId;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public long getUserId() {
+    return userId;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<String> getCategories() { return categories; }
+
+  public void setCategories(List<String> categories) { this.categories = categories; }
 }
